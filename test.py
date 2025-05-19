bf621d30b57ced7e6ee7665d3d027a8e87fd8302
Elroy Quan <elroyquan@Elroys-MacBook-Pro.local>
2025-05-19 15:52:34 -0400

    some silly testsing

---
+++ test.py
@@ -0,0 +1,5 @@
+dict = {}
+for i in range(0,10):
+    dict[i] = [{i+1:i+2}, {i:i+3}]
+    
+print (dict)
--- utscScraper.py
+++ utscScraper.py
@@ -8,10 +8,19 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 
 tables = soup.find_all('table') #returns list of all table tags 
+data = {}
+
 
 # print the first table's rows (tables[0] is the only table on the site, we use this just to be safe)
 for row in tables[0].find_all('tr'): #loop through all of the rows of the table
-    cells = row.find_all(['td']) #loop through the row's 
-    print([cell.text.strip() for cell in cells])
+    cells = row.find_all(['td']) #list of all   
+    row_text = []  # create an empty list for this row
+    for cell in cells:
+        text = cell.text.strip()
+        row_text.append(text)
+    print (row_text)
+
+
+
 
 #TODO: put the data into a dictionary ie: the room title, room num, capacity, etc
